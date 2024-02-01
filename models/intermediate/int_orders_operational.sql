b54b65ff1ff6e79f57f4359b9dd216ce4d8333e3
Saida EL BEJJAJ <s.elbejjaj@gmail.com>
2024-02-01 22:22:27 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,8 +3,10 @@ orders_id,
 date_date,
 round((margin+shipping_fee-logcost-ship_cost),2) as operational_margin,
 quantity,
+revenue,
+margin,
 from
 {{ ref('int_orders_margin') }}
 JOIN
 {{ ref('stg_raw__ship') }}
-USING(orders_id, date_date)
+USING(orders_id)
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,10 @@
+select
+    date_date,
+    round(sum(revenue),2) as total_revenue,
+    round(sum(margin),2) as total_margin,
+    count(distinct orders_id) as nb_transactions,
+    round(sum(revenue) / count(distinct orders_id), 2) as average_basket,
+    round(sum(operational_margin),2) as total_operational_margin
+from
+{{ ref('int_orders_operational') }}
+group by date_date
